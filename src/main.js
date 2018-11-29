4784d3951a5d0e9d70f2d4281768c93369369d59
MininMobile <notminin@pm.me>
2018-11-29 15:10:29 +0000

    add basic elements

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 class thing {
-	constructor(name, children) {
-		things[name] = children;
+	constructor(name, children, nick) {
+		things[name] = {
+			children: children,
+			nick: nick ? nick : [name]
+		}
 	}
 }
 
@@ -12,7 +15,8 @@ function getDropdown(type = "universe") {
 	let dropdown = document.createElement("div");
 	dropdown.classList.add("dropdown");
 	dropdown.id = `${uid}-dropdown`;
-	dropdown.innerText = type;
+	dropdown.title = `archetype: ${type}`;
+	dropdown.innerText = rarr(things[type].nick);
 
 	dropdown.onclick = () => {
 		if (things[type]) {
@@ -23,7 +27,7 @@ function getDropdown(type = "universe") {
 				child.classList.toggle("closed");
 			} else {
 				dropdown.classList.add("expanded");
-				addDropdown(dropdown, parse(things[type]));
+				addDropdown(dropdown, parse(things[type].children));
 			}
 		} else {
 			throw new Error(`Invalid type: "${type}"`);
@@ -35,16 +39,76 @@ function getDropdown(type = "universe") {
 }
 
 { // create things
-	new thing("universe", [["galactic supercluster", 20]]);
-	new thing("galactic supercluster", [["galaxy", 20, 5]]);
-	new thing("galaxy", [["galactic center", 1], ["arm", 6, 2]]);
-	new thing("galactic center", [["black hole", 1], ["star system", 30, 20]]);
-	new thing("arm", [["star system", 30, 20], ["nebula", 10, 3]]);
-	new thing("nebula", ["this is going to be complicated"]);
-	new thing("star system", ["this is going to be complicated"]);
-	new thing("black hole", [["inside the black hole", 1]]);
-	new thing("inside the black hole", [["white hole", 1]]);
-	new thing("white hole", [["universe", 1]]);
+	{ // basic elements (stolen from ortiel uwu)
+		new thing("diamond", [["carbon", 1]]);
+		new thing("oil", [["lipids", 1]]);
+		new thing("magma", [["silica", 1], [["aluminium"], 3], [["iron"], 5], [["potassium"], 5], [["sodium"], 2], [["calcium"], 2]]);
+		new thing("rock", [["silica", 1], [["aluminium"], 3], [["iron"], 5], [["potassium"], 5], [["sodium"], 2], [["calcium"], 2]]);
+		new thing("silica", [["silicon", 1], ["oxygen", 1]]);
+		new thing("chitin", [["carbon", 1], ["hydrogen", 1], ["oxygen", 1], ["nitrogen", 1]]);
+		new thing("salt", [["chlorine", 1], ["sodium", 1]]);
+		new thing("water", [["hydrogen", 1], ["oxygen"]]);
+		new thing("fire", [["oxygen", 1], ["carbon", 1]]);
+		new thing("ash", [["organic matter", 1], ["carbon", 1]]);
+		new thing("dew", [["water", 1]]);
+		new thing("ice", [["water", 1]]);
+		new thing("snow", [["snowflakes", 1]]);
+		new thing("snowflakes", [["water", 1]]);
+		new thing("ammonia", [["hydrogen", 1], ["nitrogen", 1]]);
+		new thing("methane", [["hydrogen", 1], ["carbon", 1]]);
+		new thing("hydrogen", [["proton", 1], ["electron", 1]]);
+		new thing("hydrogen atom", [["proton", 1], ["electron", 1]], ["atoms"]);
+		new thing("steel", [["iron", 1], ["carbon", 1]]);
+		new thing("plastic",	[["polymers", 1]]);
+		new thing("rubber",		[["polymers", 1]]);
+		new thing("polymers",	[["carbon", 1], ["hydrogen", 1], ["oxygen", 1]]);
+		new thing("alcohol",	[["carbon", 1], ["hydrogen", 1], ["oxygen", 1]]);
+		new thing("carbon",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("sodium",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("chlorine",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("oxygen",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("helium",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("potassium",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("aluminium",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("iron",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("copper",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("lead",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("gold",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("silver",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("silicon",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("calcium",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("nitrogen",	[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("sulfur",		[["proton", 1], ["neutron", 1], ["electron", 1]]);
+
+		new thing("glucose",		[["carbon", 1], ["hydrogen", 1], ["oxygen", 1]]);
+		new thing("atom",			[["proton", 1], ["neutron", 1], ["electron", 1]]);
+		new thing("proteins",		[["atom", 1]]);
+		new thing("lipids",			[["atom", 1]]);
+		new thing("organic matter",	[["proteins", 1], ["lipids", 1], ["glucids", 1], [["salt"], 3]]);
+		new thing("molecule",		[["atom", 1]]);
+		new thing("proton",			[["up quark", 2],["down quark", 1]]);
+		new thing("neutron",		[["down quark", 2], ["up quark", 1]]);
+		new thing("electron",		[["qwubble", 1]]);
+		new thing("up quark",		[["qwubble", 1]]);
+		new thing("down quark",		[["qwubble", 1]]);
+		new thing("qwubble",		["multiverse,1-5"]);
+		new thing("portal",			["universe"]);
+	}
+
+	{ // universe base
+		new thing("multiverse", [["universe", 30, 10]], ["multiverse","lasagnaverse","doughnutverse","towelverse","baconverse","sharkverse","nestedverse","tastyverse","upverse","downverse","layerverse","clusterverse","metaverse","quantiverse","paraverse","epiverse","alterverse","hypoverse","dimensioverse","planiverse","pluriverse","polyverse","maniverse","stackoverse","antiverse","superverse","upperverse","maxiverse","megaverse","babyverse","tinyverse","retroverse","ultraverse","topoverse","otherverse","bubbleverse","esreverse","versiverse","'verse","cookieverse","grandmaverse"]);
+		new thing("universe", [["galactic supercluster", 20]]);
+		new thing("galactic supercluster", [["galaxy", 20, 5]]);
+		new thing("galaxy", [["galactic center", 1], ["arm", 6, 2]]);
+		new thing("galactic center", [["black hole", 1], ["star system", 30, 20]]);
+		new thing("arm", [["star system", 30, 20], ["nebula", 10, 3]]);
+		new thing("black hole", [["inside the black hole", 1]]);
+		new thing("inside the black hole", [["white hole", 1]]);
+		new thing("white hole", [["universe", 1]]);
+
+		new thing("nebula", ["this is going to be complicated"]);
+		new thing("star system", ["this is going to be complicated"]);
+	}
 }
 
 { //init
@@ -81,10 +145,16 @@ function parse(childlist) {
 	let result = [];
 
 	childlist.forEach((item) => {
-		if (item[2]) {
-			result = jarr(result, starr(item[0], random(item[1], item[2])));
+		if (typeof(item[0]) == "string") {
+			if (item[2]) {
+				result = jarr(result, starr(item[0], rint(item[1], item[2])));
+			} else {
+				result = jarr(result, starr(item[0], item[1]));
+			}
 		} else {
-			result = jarr(result, starr(item[0], item[1]));
+			if (rint(item[1]) == 1) {
+				rarr(item[0]);
+			}
 		}
 	});
 
@@ -128,6 +198,15 @@ function getUID(element) {
  * @param {number} max
  * @param {number} min
  */
-function random(max, min = 1) {
+function rint(max, min = 1) {
 	return Math.floor((Math.random() * max) + min);
+}
+
+
+/**
+ * @param {array} arr
+ * @returns
+ */
+function rarr(arr){
+	return arr[Math.floor(Math.random() * arr.length)];
 }
